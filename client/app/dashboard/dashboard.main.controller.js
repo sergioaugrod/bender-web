7e325fdfc52d90a35f8aa000d49e7d359ff20876
Sérgio Rodrigues <sergioaugrod@gmail.com>
2016-01-31 22:53:24 -0200

    Adds sockets support and create sensor data visualizations

--- client/app/dashboard/dashboard.main.controller.js
+++ client/app/dashboard/dashboard.main.controller.js
@@ -9,6 +9,42 @@
 
   /* @ngInject */
   function DashboardMainController($scope, SocketService) {
-    //SocketService.connect();
+    $scope.sensors = {
+      temperature: 0,
+      luminosity: 0,
+      breathalyzer: 0
+    };
+
+    var socket = SocketService.connect();
+    var channel = SocketService.channel('sensors:data');
+
+    socket.onOpen(function(message) {
+      console.log('OPEN', message);
+    });
+
+    socket.onError(function(message) {
+      console.log('ERROR', message);
+    });
+
+    socket.onClose(function(message) {
+      console.log('CLOSE', message);
+    });
+
+    channel.on('user:entered', function(message) {
+      console.log(message);
+    });
+
+    channel.on('sensor:update', function(message) {
+      var sensor = message.topic.split("/")[1];
+
+      if(sensor == 'temperature') {
+        $scope.sensors.temperature = message.value;
+      } else if(sensor == 'luminosity') {
+        $scope.sensors.luminosity = message.value;
+      } else if(sensor == 'breathalyzer') {
+        $scope.sensors.breathalyzer = message.value;
+      }
+      $scope.$digest();
+    });
   }
 })();
--- client/app/services/services.socket.js
+++ client/app/services/services.socket.js
@@ -10,40 +10,39 @@
   /* @ngInject */
   function SocketService ($rootScope) {
     var socket = null;
-    var channel = null;
     var connected = false;
 
     var service = {
-      connect: function() {
-        socket = new Phoenix.Socket('ws://localhost:4000/socket');
-        channel = socket.channel('arduino:messages');
-        connected = true;
+      connect: function(url) {
+        var url = url || 'localhost:4000';
+
+        if (!connected) {
+          socket = new Phoenix.Socket('ws://'+ url +'/socket');
+          socket.connect();
+          connected = true;
+        }
+
+        return socket;
+      },
+      channel: function(topic) {
+        var channel = null;
+
+        if (connected && socket) {
+          channel = socket.channel(topic);
+          channel.join();
+        }
+
+        return channel;
       },
       disconnect: function() {
-        socket.disconnect();
-        connected = false;
-        socket = null;
-        channel = null;
+        if (connected) {
+          socket.disconnect();
+          connected = false;
+          socket = null;
+        }
       },
       connected: function() {
         return connected;
-      },
-      on: function(eventName, callback) {
-        if(socket) {                    
-          channel.on(eventName, function(data) {
-            $rootScope.$apply(function() {
-              callback.apply(socket, data);
-            });
-          });
-        }
-      },
-      emit: function(eventName, data) {
-        if(socket) {
-          channel.push(eventName, data);
-        }
-      },
-      getSocket: function() {
-        return socket;
       }
     };
 
